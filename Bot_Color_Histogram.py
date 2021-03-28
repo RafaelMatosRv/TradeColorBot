c35a5a1c5b9c2903edb56e877e7b0adbe385ab2f
R@f@&L M@t0$ <rafaelmatosrv@gmail.com>
2021-03-28 18:22:33 -0300

    Update Bot_Color_Histogram.py

--- Bot_Color_Histogram.py
+++ Bot_Color_Histogram.py
@@ -1,6 +1,6 @@
 #RafaelMatos
 #rafaelmatosrv@gmail.com
-#Usar ordem OCO
+
 
 import time
 
@@ -10,6 +10,14 @@ import pyautogui
 X1 = 1200    #Coordenada x da barra que será lida no histograma
 Y1 = 550     #Coordenada y da barra que será lida no histograma
 
+#X2= 11
+#Y2= 11
+
+#X3= 11
+#Y3= 11
+
+flag1 = 0
+flag2 = 0
 
 def capture_screen():  #Funcao de captura de tela
     screen = ImageGrab.grab()
@@ -17,31 +25,51 @@ def capture_screen():  #Funcao de captura de tela
 
 
 def detect_Venda(screen):
+
     color = screen.getpixel((X1, Y1))
-    if color == (0, 0, 0)   : #Verifica se a cor da barra é Preta
+    #color2 = screen.getpixel((X2, Y2))
+    #color3 = screen.getpixel((X3, Y3))
+    if color == (0, 0, 0): #Verifica se a cor da barra é Preta ## and color2(0, 0, 0) and color3(0, 0, 0) ## or color2(0, 0, 0) or color3(0, 0, 0)
         return True
+
 
 def detect_Compra(screen):
     color = screen.getpixel((X1, Y1))
-    if color == (255, 255, 255)   :    #Verifica se a cor da barra é Branca
+    #color2 = screen.getpixel((X2, Y2))
+    #color3 = screen.getpixel((X3, Y3))
+    if color == (255, 255, 255)   :    #Verifica se a cor da barra é Branca## and color2(0, 0, 0) and color3(0, 0, 0) ## or color2(0, 0, 0) or color3(0, 0, 0)
         return True
-
+    
+def detect_ResetColor(screen):
+    color = screen.getpixel((X1, Y1))
+    if color != (0, 0, 0) or color != (255, 255, 255)  :    #Verifica se a cor é diferente de branco ou diferente de preto
+        return True
 
 def Venda():
 
-    pyautogui.click(1790, 581)    #Coordenada x e y do botao venda a mercado
-    time.sleep(120)# Tempo para proxima leitura 120 segundos
+    if (flag1 == 0):
+        pyautogui.click(1790, 581)    #Coordenada x e y do botao venda a mercado
+        #time.sleep(3600)# Tempo para proxima leitura em segundos
 
 def Compra():
 
-    pyautogui.click(1620, 581)  #Coordenada x e y do botao compra a mercado
-    time.sleep(120) # Tempo para proxima leitura 120 segundos
-
+    if (flag2 == 0):
+        pyautogui.click(1620, 581)  #Coordenada x e y do botao compra a mercado
+        #time.sleep(120) # Tempo para proxima leitura 120 segundos
 
 
 while True:
     screen = capture_screen()
     if detect_Venda(screen):
         Venda()
+        flag1 = 1
+
+
+
     elif detect_Compra(screen):
         Compra()
+        flag2 = 1
+
+    elif detect_ResetColor(screen):
+        flag1 = 0
+        flag2 = 0
